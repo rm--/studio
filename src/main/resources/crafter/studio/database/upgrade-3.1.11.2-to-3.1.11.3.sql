fc5d85af8176984845b2bc432544e9c74acb164d
Sumer Jabri <sumer.jabri@craftercms.org>
2022-05-12 16:45:22 -0400

    Updated to v4.0.0

--- src/main/resources/crafter/studio/database/upgrade-3.1.11.2-to-3.1.11.3.sql
+++
@@ -1,7 +0,0 @@
-DROP PROCEDURE tryLockSyncRepoForSite ;
-
-call dropColumnIfExists('crafter', 'site', 'sync_repo_lock_owner') ;
-
-call dropColumnIfExists('crafter', 'site', 'sync_repo_lock_heartbeat') ;
-
-UPDATE _meta SET version = '3.1.11.3' ;
--- src/main/resources/crafter/studio/database/upgrade-3.1.9.3-to-3.1.10.1.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `site` ADD COLUMN `publishing_lock_owner` VARCHAR(255) NULL ;
-
-ALTER TABLE `site` ADD COLUMN `publishing_lock_heartbeat` DATETIME NULL ;
-
-UPDATE _meta SET version = '3.1.10.1' ;
